ba85a3bef1c369f498d2eeb8ff30c905bc648321
Caio Damiao <flongcd@gmail.com>
2025-01-08 01:45:27 -0300

    feat: criacao do servico com metodos de busca e cadastro

---
+++ src/main/java/com/flong/ecommerce/usuarios/UsuariosServico.java
@@ -0,0 +1,39 @@
+package com.flong.ecommerce.usuarios;
+
+import com.flong.ecommerce.exception.EntidadeExistenteException;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import static com.flong.ecommerce.usuarios.UsuariosConversor.toUsuarios;
+import static com.flong.ecommerce.usuarios.UsuariosEspecificacao.*;
+import static org.springframework.data.jpa.domain.Specification.where;
+
+@Service
+public class UsuariosServico {
+
+    private final UsuariosRepositorio usuariosRepositorio;
+
+    public UsuariosServico(UsuariosRepositorio usuariosRepositorio) {
+        this.usuariosRepositorio = usuariosRepositorio;
+    }
+
+    @Transactional
+    public Page<Usuarios> buscarUsuarios(UsuariosFiltro filtro, Pageable pageable) {
+        return usuariosRepositorio.findAll(where(porNome(filtro))
+                .and(porEmail(filtro))
+                .and(porId(filtro))
+                .and(porDataCriacao(filtro)), pageable);
+    }
+
+    @Transactional
+    public Usuarios cadastrarUsuario(UsuariosDto dto) {
+        try {
+            return usuariosRepositorio.save(toUsuarios(dto));
+        }
+        catch (Exception e){
+            throw new EntidadeExistenteException("Usuário já cadastrado");
+        }
+    }
+}
